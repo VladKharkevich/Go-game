11049e9912f51742dd4b4c4dfd4292af1df8924d
Vlad <kharkevichvlad@gmail.com>
2019-07-20 22:21:45 +0300

    third commit

--- go.py
+++ go.py
@@ -6,8 +6,12 @@ display_size = (800, 600)
 display = pygame.display.set_mode(display_size)
 pygame.display.set_caption('GO')
 clock = pygame.time.Clock()
-make_jump = False
 turn = True
+color = {
+    'black': (0, 0, 0),
+    'white': (255, 255, 255),
+    'brown': (0xcd, 0x85, 0x3f)
+}
 
 
 class Board:
@@ -25,8 +29,15 @@ def rungame():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        display.fill((255, 255, 255))
+        display.fill(color['white'])
         draw_board(go_board)
+        font = pygame.font.Font(None, 72)
+        if turn:
+            text = font.render("Black's turn", 1, (0, 100, 0))
+        else:
+            text = font.render("White's turn", 1, (0, 100, 0))
+        place = text.get_rect(center=(400, 50))
+        display.blit(text, place)
         click = pygame.mouse.get_pressed()
         if click[0] == 1:
             make_step(go_board)
@@ -35,21 +46,21 @@ def rungame():
 
 
 def draw_board(go_board):
-    pygame.draw.rect(display, (0xcd, 0x85, 0x3f),
-                     (150, 50, 30 + 18 * 25, 30 + 18 * 25))
+    pygame.draw.rect(display, color['brown'],
+                     (150, 80, 30 + 18 * 25, 30 + 18 * 25))
     for i in range(19):
-        pygame.draw.line(display, (0, 0, 0),
-                         (165 + 25 * i, 65), (165 + 25 * i, 515), 3)
-        pygame.draw.line(display, (0, 0, 0),
-                         (165, 65 + 25 * i), (615, 65 + 25 * i), 3)
+        pygame.draw.line(display, color['black'],
+                         (165 + 25 * i, 95), (165 + 25 * i, 545), 3)
+        pygame.draw.line(display, color['black'],
+                         (165, 95 + 25 * i), (615, 95 + 25 * i), 3)
     for i in range(19):
         for j in range(19):
             if go_board.board[i][j] == 'black':
-                pygame.draw.circle(display, (0, 0, 0),
-                                   (165 + 25 * i, 65 + 25 * j), 12)
+                pygame.draw.circle(display, color['black'],
+                                   (165 + 25 * i, 95 + 25 * j), 12)
             elif go_board.board[i][j] == 'white':
-                pygame.draw.circle(display, (255, 255, 255),
-                                   (165 + 25 * i, 65 + 25 * j), 12)
+                pygame.draw.circle(display, color['white'],
+                                   (165 + 25 * i, 95 + 25 * j), 12)
 
 
 def make_step(go_board):
@@ -65,10 +76,9 @@ def make_step(go_board):
 
 
 def find_coordinates(pos, go_board):
-    if (pos[0] in range(160, 621)) and (pos[1] in range(60, 521)) and (
-            pos[0] % 25 > 10 or pos[0] % 25 < 20) and (
-            pos[1] % 25 > 10 or pos[1] % 25 < 20):
-        coord = [(pos[0] - 160) // 25, (pos[1] - 60) // 25]
+    if (pos[0] in range(160, 621)) and (pos[1] in range(90, 551)) and (
+            pos[0] % 25 > 10 or pos[0] % 25 < 20) and (pos[1] % 25 > 15):
+        coord = [(pos[0] - 160) // 25, (pos[1] - 90) // 25]
         if go_board.board[coord[0]][coord[1]] == 'blank':
             return coord
     return None
